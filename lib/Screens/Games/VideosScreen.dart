2478cc57952c08cc957b0c77ff0c2391644676e6
Mostafa Fahimi <mostafafahimi73@gmail.com>
2021-03-01 16:58:41 +0330

    Not gonna add video page yet.

--- lib/Screens/Games/VideosScreen.dart
+++ lib/Screens/Games/VideosScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+///for test
 class VideosScreen extends StatefulWidget {
   @override
   _VideosScreenState createState() => _VideosScreenState();
